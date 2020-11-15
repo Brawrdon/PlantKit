667e9ebb4adc2c14aa74d49016eea21213c1663f
Brawrdon <BrandonOkeke@icloud.com>
2020-11-15 21:58:12 +0000

    feat: add restart when WiFi is configured

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Wifi.h>
-#include <WebServer.h> 
+#include <WebServer.h>
 #include <AutoConnect.h>
 
 WebServer server; 
@@ -9,26 +9,21 @@ AutoConnect portal(server);
 AutoConnectConfig config;
 
 
-void rootPage(){
-	char content[] = "Hello, world";
-	server.send(200, "text/plain", content);
-}
-
 void setup(){
 	delay(1000);
 	Serial.begin(115200);
-	Serial.println();
-
-	// Server routes
-	server.on("/", rootPage);
-
 	// Portal config
 	config.apid = "PlantKit";
 	config.psk = "plantparty";
 	config.hostName = "PlantKit";
 	portal.config(config);
+
 	if (portal.begin()) {
 		Serial.println("WiFi connected: " + WiFi.localIP().toString());
+
+		if (WiFi.getMode() == WIFI_MODE_APSTA) {
+			ESP.restart();
+		}
 	}
 }
 
